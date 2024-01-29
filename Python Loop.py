11c6a53c19f5b01c7c7abcbb1fc4b6006d817a5d
marcelomaeda <marcelomaeda9@gmail.com>
2024-01-29 14:10:34 +0000

    Python Loop Jupyter Notebook
    Python Loop Jupyter Notebook

---
+++ Python Loop.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Loop
+
+
+# In[2]:
+
+
+i=1
+
+
+# In[3]:
+
+
+while i<=10:
+    print(i)
+    i=i+1
+
+
+# In[4]:
+
+
+i = 1
+
+
+# In[5]:
+
+
+n = 2
+
+
+# In[6]:
+
+
+while i<=10:
+    print(n, " * ", i, " = ", n*i)
+    i = i+1
+
+
+# In[7]:
+
+
+l1 = [1,2,3,4,5]
+
+
+# In[8]:
+
+
+i = 0
+
+
+# In[9]:
+
+
+while i < len(l1):
+    l1[i]=l1[i]+100
+    i=i+1
+
+
+# In[10]:
+
+
+l1
+
+
+# In[11]:
+
+
+l1 = ["apple", "banana", "orange"]
+
+
+# In[12]:
+
+
+for i in l1:
+    print(i)
+
+
+# In[13]:
+
+
+l1 = ["orange", "blue", "green"]
+l2 = ["book", "chair", "phone"]
+
+
+# In[14]:
+
+
+for i in l1:
+    for j in l2:
+        print(i,j)
+
+
+# In[ ]:
+
+
+
+
