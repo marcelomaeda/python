e418e0f8a372149f47af744c30c78ded583f6f29
marcelomaeda <marcelomaeda9@gmail.com>
2024-01-29 15:31:43 +0000

    Python Tokens
    Python Tokens

---
+++ Python Tokens.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Tokens
+
+
+# In[2]:
+
+
+name = "I am Marcelo"
+
+
+# In[3]:
+
+
+Name = "Am I Marcelo"
+
+
+# In[4]:
+
+
+name
+
+
+# In[5]:
+
+
+Name
+
+
+# In[6]:
+
+
+a1 = "Movie"
+
+
+# In[7]:
+
+
+a1 = 123
+
+
+# In[8]:
+
+
+a1 = True
+
+
+# In[ ]:
+
+
+
+
