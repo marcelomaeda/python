cb03876ee3fad43cba5e7ab8c5336f779a08e477
marcelomaeda <marcelomaeda9@gmail.com>
2024-01-29 15:14:05 +0000

    Python Tuple
    Python Tuple

---
+++ Python Tuple.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Tuple
+
+
+# In[2]:
+
+
+tup1 = (1,"a",True,2,"b",False)
+
+
+# In[3]:
+
+
+tup1
+
+
+# In[4]:
+
+
+type(tup1)
+
+
+# In[5]:
+
+
+tup1
+
+
+# In[6]:
+
+
+tup1[0]
+
+
+# In[7]:
+
+
+tup1[1]
+
+
+# In[8]:
+
+
+tup1[-1]
+
+
+# In[9]:
+
+
+tup1[2:5]
+
+
+# In[10]:
+
+
+tup1
+
+
+# In[11]:
+
+
+tup1[2] = "Hi"
+
+
+# In[12]:
+
+
+tup1[6] = "movie"
+
+
+# In[13]:
+
+
+tup1
+
+
+# In[14]:
+
+
+len(tup1)
+
+
+# In[16]:
+
+
+tup1 = ("a",True,3+5j)
+tup2 = ("movie",False,5050)
+
+
+# In[17]:
+
+
+tup1+tup2
+
+
+# In[18]:
+
+
+tup2 + tup1
+
+
+# In[19]:
+
+
+tup1 = (1,"True",3.14)
+
+
+# In[20]:
+
+
+tup1*3
+
+
+# In[21]:
+
+
+tup2 = ("a","b","c")
+
+
+# In[22]:
+
+
+tup1*5 + tup2
+
+
+# In[23]:
+
+
+tup1 = (67,43,1,90,-45,68)
+
+
+# In[24]:
+
+
+min(tup1)
+
+
+# In[25]:
+
+
+max(tup1)
+
+
+# In[ ]:
+
+
+
+
