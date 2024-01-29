ddce1d9dc806f1996d83126d4eeeb0aa41decd3b
marcelomaeda <marcelomaeda9@gmail.com>
2024-01-29 15:23:34 +0000

    Python Strings
    Python Strings

---
+++ Python Strings.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s1 = "Hi"
+
+
+# In[2]:
+
+
+s1
+
+
+# In[3]:
+
+
+type(s1)
+
+
+# In[4]:
+
+
+s1 = "Movie"
+s1
+
+
+# In[5]:
+
+
+type(s1)
+
+
+# In[6]:
+
+
+s1 = '''multipleline
+string
+'''
+
+
+# In[7]:
+
+
+s1
+
+
+# In[8]:
+
+
+my_string = "My name is Marcelo Maeda"
+
+
+# In[9]:
+
+
+my_string[0]
+
+
+# In[10]:
+
+
+my_string[-1]
+
+
+# In[11]:
+
+
+my_string[4]
+
+
+# In[12]:
+
+
+my_string[11:15]
+
+
+# In[13]:
+
+
+my_string
+
+
+# In[14]:
+
+
+len(my_string)
+
+
+# In[15]:
+
+
+my_string.upper()
+
+
+# In[16]:
+
+
+my_string.lower()
+
+
+# In[17]:
+
+
+my_string.replace('John','Sam')
+
+
+# In[18]:
+
+
+my_string.count("name")
+
+
+# In[19]:
+
+
+my_string.find("m")
+
+
+# In[20]:
+
+
+my_string.split(' ')
+
+
+# In[ ]:
+
+
+
+
